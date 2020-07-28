33c45d51e42ea5ce21962fc1d60c4b0b64a4ca07
Ajay Kumar Singh <ajaykumar.kumar900@gmail.com>
2020-07-28 21:15:57 +0530

    added shimmer animation:

--- lib/components/custom_map_user_card.dart
+++ lib/components/custom_map_user_card.dart
@@ -1,25 +1,184 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:rent_app/const.dart';
+import 'package:shimmer/shimmer.dart';
 
 class CustomMapUserCard extends StatelessWidget {
+  // We wll need the user uid only to fetch data from firebase
   final String uid;
 
-  const CustomMapUserCard({Key key, this.uid}) : super(key: key);
-
   // Use provider here
+  static bool isLoaded = false;
+
+
+
+  const CustomMapUserCard({Key key, this.uid}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: new Text("Center"),
+      body: isLoaded ? Padding(
+        padding: const EdgeInsets.all(18.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                new Text(
+                  "Name goes here",
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                ),
+                new CircleAvatar(
+                  backgroundImage: NetworkImage(Constants().defaultImageUrl),
+                )
+              ],
+            ),
+
+            Padding(
+              padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+              child: new Text("Gallery",style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+            ),
+            galleryWidget(context),
+            Padding(
+              padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+              child: new Text("Email Id",style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+            ),
+          ],
+        ),
+      ) :Padding(
+        padding: const EdgeInsets.all(18.0),
+        child: ListView(
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                placeHolderNameWidget(),
+                placeHolderProfileWidget()
+              ],
+            ),
+
+            Padding(
+              padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+              child: new Text("Gallery",style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+            ),
+            placeHolderGalleryWidget(context),
+            Padding(
+              padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+              child: new Text("Email Id",style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+
+            ),
+            placeHolderNameWidget(),
+            Padding(
+              padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+              child: new Text("Address",style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+            ),
+            placeHolderNameWidget(),
+            new SizedBox(
+              height: 10,
+            ),
+            placeHolderNameSecondaryWidget()
+          ],
+        ),
       ),
-      floatingActionButton: new FloatingActionButton.extended(
-        onPressed: () {
-          print("Call");
-        },
-        label: Icon(Icons.call),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: new FloatingActionButton.extended(
+          backgroundColor: Colors.indigoAccent,
+          onPressed: () {
+            print("Call");
+          },
+          label: Icon(Icons.call),
+        ),
+      ),
+    );
+  }
+
+  Widget galleryWidget(BuildContext ctx) {
+    return Container(
+      height: MediaQuery.of(ctx).size.height * 0.5,
+      child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, pos) {
+            return Container(
+              width: 180,
+              child: Card(
+                child: Center(child: new Text(pos.toString())),
+              ),
+            );
+          }),
+    );
+  }
+
+  Widget placeHolderNameWidget(){
+    return Container(
+      child: Shimmer.fromColors(
+        baseColor: Constants().baseColor,
+        highlightColor: Constants().highlightColor,
+        child:  Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(20)
+          ),
+          width: 220,
+          height: 28.0,
+        )
+    ));
+  }
+  Widget placeHolderNameSecondaryWidget()
+  {
+    return Container(
+        child: Shimmer.fromColors(
+            baseColor: Constants().baseColor,
+            highlightColor: Constants().highlightColor,
+            child:  Container(
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20)
+              ),
+              width: 260,
+              height: 28.0,
+            )
+        ));
+  }
+
+  Widget placeHolderProfileWidget(){
+    return Container(
+        child: Shimmer.fromColors(
+            baseColor: Constants().baseColor,
+            highlightColor: Constants().highlightColor,
+            child:  CircleAvatar(
+              backgroundColor: Colors.white,
+            )
+        ));
+  }
+
+  Widget placeHolderGalleryWidget(BuildContext ctx){
+    return Container(
+      height: MediaQuery.of(ctx).size.height * 0.5,
+      child: Shimmer.fromColors(
+        baseColor: Constants().baseColor,
+        highlightColor: Constants().highlightColor,
+        child: ListView.builder(
+
+            scrollDirection: Axis.horizontal,
+            itemBuilder: (context, pos) {
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  width: 180,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10)
+                  ),
+                  child: Card(
+                    clipBehavior: Clip.antiAlias,
+                    child: Center(child: new Text(pos.toString())),
+                  ),
+                ),
+              );
+            }),
       ),
     );
   }
--- lib/const.dart
+++ lib/const.dart
@@ -1,4 +1,9 @@
+import 'package:flutter/material.dart';
+
 class Constants {
+  Color baseColor = Colors.grey[300];
+  Color highlightColor = Colors.grey[100];
+
   final String defaultImageUrl =
-      "https://lh3.googleusercontent.com/proxy/QHe_ary1oUuwW70XkuxGzS8Gy5r0eUAi7LWC_1jurjdlQp5NR9g8TmfhF9BR2ALFOxax65BtF4DUu0jJkFJ2cGsajgw5F9PzBMtzk8KKp4_-zpVfoCIa";
+      "https://www.iwmbuzz.com/wp-content/uploads/2020/01/hot-instagram-pictures-of-parth-samthaan-see-pics-920x518.jpg";
 }
